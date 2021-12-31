2a6d5330b195f3045c66cdbfe7a6fef775727de8
ayznnn <67419505+AYZNN@users.noreply.github.com>
2021-12-31 04:38:08 +0100

    Update menu - Copie.lua

--- menu - Copie.lua
+++ menu - Copie.lua
@@ -1,1 +1,39 @@
 
+**Description**
+WaveProperties is an amazing ESX & QBUS ready resource, allowing you to create properties including garages.With WaveProperties your players can manage anything imaginable.❤️ ( see video )
+Every features is configurable and can be adapted to all your tastes! 
+
+**Showcase**
+Watch the [Showcase](https://www.youtube.com/watch?v=B_MvXP3gz9w) !
+
+**Tebex**
+[WaveResources | Home (tebex.io)](https://waveresources.tebex.io/) - 50€ + taxes
+
+**Features**
+* Use job or Whitelisted identifiers ( to create,edit, or delete properties )
+* Property Creator
+  * Set up Name,Price, Max  number of players that can OWN the property
+  * Set garage limited vehicle spots
+  * ~10 ready Interiors
+  * Store money, items, weapons
+  * Save outfits
+* Garage System
+  * GTA:O style, using scaleforms to display stats & upgrades
+  * Upgrades are saved
+  * Configure if they keep damages or not
+* Keys System
+  * Give and Delete Keys that you gave
+  * Manage tenants accesses ( Garage,Storage,Garage)
+  * If they have the key, they can have access to your saved datas ( if you allowed them ) 
+* Visit, Rent, Resell ( with taxes ) and Purchase Properties 
+* Ringing doorbell
+* Anti-Glitchs ( no duplication )
+* Nice Blips ( shows you if  you own or if the property is for sale )
+* Nice sounds
+* Nices Notifications
+* Nice scenes
+* Property  Furnitures [WIP]
+* Property Robberies [WIP]
+* built-in RageUI edited
+* Multiples Languages supported
+* ESX & QBUS Ready
